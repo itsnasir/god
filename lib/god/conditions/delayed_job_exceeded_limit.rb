1798a5e83f07e0f39fa107ce0483a86debd2fd10
fawad <fawad@fawad-laptop.(none)>
2012-10-31 22:29:19 +0500

    change in limit exceed condition class

--- lib/god/conditions/delayed_job_exceeded_limit.rb
+++ lib/god/conditions/delayed_job_exceeded_limit.rb
@@ -1,40 +1,12 @@
 module God
   module Conditions
 
-    # Condition Symbol :delayed_job_exceeded_limit
-    # Type: Poll
-    #
-    # Trigger when the resident delayed job processes is above a specified limit.
-    #
-    # Paramaters
-    #   Required
-    #     +pid_file+ is the pid file of the process in question. Automatically
-    #                populated for Watches.
-    #     +above+ is the amount of resident delayed job processes above which
-    #             the condition should trigger.
-    #
-    # Examples
-    #
-    # Trigger if the process is using more than 100 megabytes of resident
-    # memory (from a Watch):
-    #
-    #   on.condition(:memory_usage) do |c|
-    #     c.above = 100.megabytes
-    #   end
-    #
-    # Non-Watch Tasks must specify a PID file:
-    #
-    #   on.condition(:memory_usage) do |c|
-    #     c.above = 100.megabytes
-    #     c.pid_file = "/var/run/mongrel.3000.pid"
-    #   end
     class DelayedJobExceededLimit < PollCondition
-      attr_accessor :above, :pid_file
+      attr_accessor :above, :pid_file, :env_file
 
       def initialize
         super
         self.above = nil
-        self.no_of_records = Delayed::Job.count
       end
 
       def pid
@@ -45,14 +17,17 @@ module God
         valid = true
         valid &= complain("Attribute 'pid_file' must be specified", self) if self.pid_file.nil? && self.watch.pid_file.nil?
         valid &= complain("Attribute 'above' must be specified", self) if self.above.nil?
+        valid &= complain("Attribute 'env_file' must be specified", self) if self.env_file.nil?
         valid
       end
 
       def test
-        self.info = []
+        require self.env_file
 
-        if self.no_of_records > self.above
-          self.info = "delayed job exceeds limit #{self.above}"
+        no_of_records = Delayed::Job.count
+
+        if no_of_records > above
+          self.info = "delayed job exceeds limit #{above}"
           return true
         else
           return false
